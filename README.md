1371c1ddb07d3ece06c1582d7cbcb233fa087bf9
Thybow <tmilville.pro@gmail.com>
2023-09-07 17:36:29 +0200

    update / prettier code and explanations

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Test to create a Node.js server.
 
 You need to open 'localhost:8080' to be able to test it.
+
+To test it with a URL parameter, you can open 'localhost:8080?name=yourName'.
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 http = require('http');
-// File system
+// File system and url modules
 fs = require('fs');
 url = require('url');
 
@@ -8,14 +8,20 @@ http.createServer((req, res) => {
   //console.log(url.parse(req.url, true).query.nom);
   fs.readFile('index.html', 'utf-8', (err, data) => {
     if (err) {
+      // Manage error
       res.writeHead(404);
       res.write('File not found');
     }
     else {
+      // Write content of the HTML file and replace the **** with the name
       res.writeHead(200, {
         'Content-Type': 'text/html, charset=utf-8'
       });
-      res.write(data.replace('****', url.parse(req.url, true).query.nom));
+      if(url.parse(req.url, true).query.nom == undefined)
+        res.write(data.replace('*****', ''));
+      else {
+        res.write(data.replace('*****', url.parse(req.url, true).query.nom));
+      }
     }
     res.end();
   });
