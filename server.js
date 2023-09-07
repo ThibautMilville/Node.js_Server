a27844f95c917ad08b9c5471f9ebb5dd054e764a
Thybow <tmilville.pro@gmail.com>
2023-09-07 16:38:22 +0200

    send html | json

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ http = require('http');
 http.createServer((req, res) => {
   // Set response header
   res.writeHead(200, {
-    'Content-Type': 'text/html, charset=utf-8'
+    'Content-Type': 'text/json'
   });
   // Write response in the terminal
   console.log('Request received');
   // Write response in the browser
-  res.write('Hello <b>World</b>!');
+  res.write('{ "name": "John", "age": 30, "city": "New York"}')
   // End response
   res.end();
 }).listen(8080, () => {
