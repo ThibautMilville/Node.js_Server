15c39b058835fd5ef4586cc21c6c84802d5654b6
Thybow <tmilville.pro@gmail.com>
2023-09-07 16:33:26 +0200

    node server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 http = require('http');
 
-server = http.createServer();
-
-server.listen(8080)
+// Create server
+http.createServer((req, res) => {
+  // Set response header
+  res.writeHead(200, {
+    'Content-Type': 'text/html, charset=utf-8'
+  });
+  // Write response in the terminal
+  console.log('Request received');
+  // Write response in the browser
+  res.write('Hello <b>World</b>!');
+  // End response
+  res.end();
+}).listen(8080, () => {
+  console.log('Server is running...');
+});
